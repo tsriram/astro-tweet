4bbed7516e3772e5f54649d0097c9e8b70e8d816
Sriram Thiagarajan <sri.sjc@gmail.com>
2023-11-06 11:40:40 +0530

    remove unused import

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from "astro/config";
-import nodejs from "@astrojs/node";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
